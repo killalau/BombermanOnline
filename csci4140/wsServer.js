87e4c4c2ca6a4f6368b89ff1117b8ee44455d4d7
Andy Kan <andy0hk2012@yahoo.com.hk>
2013-05-10 23:20:05 +0800

    php formatting

--- csci4140/wsServer.js
+++ csci4140/wsServer.js
@@ -7,6 +7,14 @@ var gameServer = GameServer.create();
 function start(httpServer, route, handlers){
 	var wsServer = new WebSocketServer({
 		httpServer: httpServer
+		
+		//Andy: I found this, not sure it is important or not
+			// You should not use autoAcceptConnections for production
+			// applications, as it defeats all standard cross-origin protection
+			// facilities built into the protocol and the browser.  You should
+			// *always* verify the connection's origin and decide whether or not
+			// to accept it.
+		//autoAcceptConnections: false
 	});
 
 	wsServer.on('request', function(request){
