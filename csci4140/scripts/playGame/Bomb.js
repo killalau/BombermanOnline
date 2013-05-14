c1c0da249ae2c877f7951061d00aff86a9f0a3ba
MondWan <victor2wen@gmail.com>
2013-05-14 22:00:38 +0800

    New Feature
    [Feature] add animation on Bomb.explode()
    PS. remember to give at least 2 block on each side for it to explode (DO
    NOT OUTBOUND)

--- csci4140/scripts/playGame/Bomb.js
+++ csci4140/scripts/playGame/Bomb.js
@@ -69,6 +69,40 @@ BMO.Bomb.prototype.setView = function(_id){
 BMO.Bomb.prototype.explode = function(payload){
 	try{
 	console.log("Bomb.explode():payload=",payload);
+	var _fire = new BMO.Fire(this.grid,this.BMM,this.wsClient); 
+	var _grid = this.grid;
+	_fire.setView("fire_c");
+	_grid.addElement(_fire);
+	_grid.view.addChild(_fire.view);
+	for(var _direction in payload){
+		console.log("Bomb.explode()._dir=",_direction);
+		for(var i =0;i<payload[_direction].length;i++){
+			if ( _direction === "U" ){
+					_grid = this.BMM.gridList[this.grid.Y-1-i][this.grid.X];
+					_fire = new BMO.Fire(_grid,this.BMM,this.wsClient);
+					if (i != (payload[_direction].length -1 )) _fire.setView("fire_v");
+					else _fire.setView("fire_u");
+			}else if (_direction === "D" ){
+					_grid = this.BMM.gridList[this.grid.Y+1+i][this.grid.X];
+					_fire = new BMO.Fire(_grid,this.BMM,this.wsClient);
+					if (i != (payload[_direction].length -1 )) _fire.setView("fire_v");
+					else _fire.setView("fire_d");
+			}else if (_direction === "L" ){
+					_grid = this.BMM.gridList[this.grid.Y][this.grid.X-1-i];
+					_fire = new BMO.Fire(_grid,this.BMM,this.wsClient);
+					if (i != (payload[_direction].length -1 )) _fire.setView("fire_h");
+					else _fire.setView("fire_l");
+			}else if (_direction === "R" ){
+					_grid = this.BMM.gridList[this.grid.Y][this.grid.X+1+i];
+					_fire = new BMO.Fire(_grid,this.BMM,this.wsClient);
+					if (i != (payload[_direction].length -1 )) _fire.setView("fire_h");
+					else _fire.setView("fire_r");
+			}
+			_grid.addElement(_fire);
+			_grid.view.addChild(_fire.view);
+		}
+
+	}
 	if (this.owner.alive) this.owner.bombNum++;
 	this.vanish();
 	}catch(e){console.log(e);throw e;};
---
+++ csci4140/scripts/playGame/Fire.js
@@ -0,0 +1,45 @@
+var BMO = window.BMO ? window.BMO : {};
+
+/*
+@class: Fire
+@extends: Element
+@construtor
+@param _grid: BMO.Grid
+@param _BMM: BMO.BMM
+@param _wsClient: BMO.BMM.wsClient
+**/
+BMO.Fire =function(_grid,_BMM,wsClient){
+	try{
+	BMO.Element.call(this,_grid,_BMM);
+	this.wsClient = wsClient;
+	this.classname = "Fire";
+	}catch(e){throw e;};
+}
+
+//constructor
+BMO.Fire.construtor = BMO.Fire;
+BMO.Fire.prototype = Object.create( BMO.Element.prototype );
+
+/*
+@public overrid method setView
+@param _id: frame_id in .json
+**/
+BMO.Fire.prototype.setView = function(_id){
+	BMO.Element.prototype.setView.call(this,_id);//super.setView()....
+	var self = this;
+	setTimeout(function(){
+		console.log(_id+"1");
+		BMO.Element.prototype.setView.call(self,_id+"1");
+		setTimeout(function(){self.vanish();},200);
+	},200);
+}
+
+/*
+@protected override method vanish()
+**/
+BMO.Fire.prototype.vanish = function(){
+        try{
+        BMO.Element.prototype.vanish.call(this);
+        this.wsClient= null;
+        }catch(e){console.log(e);throw e;};
+}
