06b4b828133e6e7c2fde820ddf620cdafd1a0f53
Anthonysko <anthonysko@gmail.com>
2013-05-29 03:07:48 +0800

    change

--- csci4140/scripts/gameroom_action.js
+++ csci4140/scripts/gameroom_action.js
@@ -63,16 +63,16 @@ function map_change(e){
 	var map_div = document.getElementById("map_choose");
 	map_div.innerHTML = '';
 	var new_img = document.createElement("img");
+	
 	if(action == "map_back")
-	{
-		//need xhr, later add		
+	{	
 		setAttributes(new_img, {"id": "map_img", "class": "map_img", "src": "../images/map1.jpg"});
 		map_div.appendChild(new_img);
 		
 	}
 	else if(action == "map_next")
 	{
-		setAttributes(new_img, {"id": "map_img", "class": "map_img", "src": "../images/map2.jpg"});
+		setAttributes(new_img, {"id": "map_img", "class": "map_img", "src": "../images/map1.jpg"});
 		map_div.appendChild(new_img);
 	}
 }
